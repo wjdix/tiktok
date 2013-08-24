62c6f90e2e6860fed74fb9b50221deb260ae67aa
William Dix <william.j.dix@gmail.com>
2013-08-24 13:44:03 -0500

    Initial pass at a controllable ticker

---
+++ tiktok.go
@@ -0,0 +1,30 @@
+package tiktok
+
+import (
+	"time"
+)
+
+type ControllableTicker struct {
+	C        <-chan time.Time
+	c        chan time.Time
+	interval int
+	elapsed  int
+}
+
+func NewTicker(d int) *ControllableTicker {
+	c := make(chan time.Time)
+	return &ControllableTicker{
+		C:        c,
+		interval: d,
+		c:        c,
+		elapsed:  0,
+	}
+}
+
+func (ticker *ControllableTicker) Tick(d int) {
+	ticker.elapsed = d + ticker.elapsed
+	if ticker.elapsed >= ticker.interval {
+		ticker.elapsed = ticker.elapsed - ticker.interval
+		ticker.c <- time.Now()
+	}
+}
---
+++ tiktok_test.go
@@ -0,0 +1,70 @@
+package tiktok
+
+import (
+	"github.com/benmills/quiz"
+	"testing"
+	"time"
+)
+
+func countTicks(tickChan <-chan time.Time, shutDownChan chan int, returnChan chan int) {
+	ticks := 0
+	for {
+		select {
+		case <-tickChan:
+			ticks += 1
+		case <-shutDownChan:
+			returnChan <- ticks
+			return
+		}
+	}
+}
+
+func TestTickerDoesNotTickEarly(t *testing.T) {
+	test := quiz.Test(t)
+	shutDownChannel := make(chan int)
+	finishedChannel := make(chan int)
+	ticker := NewTicker(5)
+	go countTicks(ticker.C, shutDownChannel, finishedChannel)
+
+	ticker.Tick(3)
+	var ticks int
+	shutDownChannel <- 0
+	select {
+	case ticks = <-finishedChannel:
+		test.Expect(ticks).ToEqual(0)
+	}
+}
+
+func TestTickerTicksAfterDuration(t *testing.T) {
+	test := quiz.Test(t)
+	shutDownChannel := make(chan int)
+	finishedChannel := make(chan int)
+	ticker := NewTicker(5)
+	go countTicks(ticker.C, shutDownChannel, finishedChannel)
+
+	ticker.Tick(5)
+	var ticks int
+	shutDownChannel <- 0
+	select {
+	case ticks = <-finishedChannel:
+		test.Expect(ticks).ToEqual(1)
+	}
+}
+
+func TestTickerTicksMultipleTimes(t *testing.T) {
+	test := quiz.Test(t)
+	shutDownChannel := make(chan int)
+	finishedChannel := make(chan int)
+	ticker := NewTicker(5)
+	go countTicks(ticker.C, shutDownChannel, finishedChannel)
+
+	ticker.Tick(6)
+	ticker.Tick(4)
+	var ticks int
+
+	shutDownChannel <- 0
+	select {
+	case ticks = <-finishedChannel:
+		test.Expect(ticks).ToEqual(2)
+	}
+}
